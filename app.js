ef8a1cb5ed670e3326228a30a08512e802db96f3
Yassine <progyss9@gmail.com>
2022-07-03 17:10:32 +0100

    .

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+let playerScore = 0;
+let computerScore = 0;
+const pScore = document.querySelector('.playerScore');
+const cScore = document.querySelector('.computerScore');
+const comment = document.querySelector('.comment');
 
 function computerSelect(){
   let selection = Math.floor(Math.random() * 3);
@@ -10,112 +18,45 @@ function computerSelect(){
       return 'scissors'
   }
 };
-const comment = document.querySelector('.comment');
 
-function playRound(computer,player){
-  /* const comment = document.querySelector('.comment'); */
-  
-  if (computer === player) {
-    comment.textContent = 'Tie Game'
-    return 'tie'
-  };
-  switch (player){
-    case 'rock':
-      if (computer === 'paper') {
-        comment.textContent = 'You Lose! Paper beats Rock'
-        return 'lost'
-      } else {
-        comment.textContent = 'You Win! Rock beats Scissors'
-        return 'win'
-      };
-    
-    case 'paper':
-      if (computer === 'scissors') {
-        comment.textContent = 'You Lose! Scissors beats Paper'
-        return 'lost'
-      } else {
-        comment.textContent = 'You Win! Paper beats Rock'
-        return 'win'
-      };
-    
-    case 'scissors':
-      if (computer === 'rock') {
-        comment.textContent = 'You Lose! Rock beats Scissors'
-        return 'lost'
-      } else {
-        comment.textContent = 'You Win! Scissors beats Paper'
-        return 'win'
-      };
-  };
-};
-
-let playerScore = 0;
-let computerScore = 0;
-const pScore = document.querySelector('.playerScore');
-const cScore = document.querySelector('.computerScore')
-
-pScore.textContent = '0';
-cScore.textContent = '0';
-
-function getScore(game){
-
-  if (game === 'win') {
-    playerScore++;
-    pScore.textContent = `${playerScore}`
+function playRound(playerSelection){
+  computerSelection = computerSelect();
+  console.log('player : '+playerSelection)
+  console.log('computer : '+computerSelection)
+  switch(playerSelection+' vs '+computerSelection){
+    case 'rock vs scissors':
+    case 'paper vs rock':
+    case 'scissors vs paper':
+      console.log('YOU WIN');
+      playerScore++;
+      pScore.textContent = `${playerScore}`;
+      comment.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+      break;
+    case 'rock vs paper':
+    case 'paper vs scissors':
+    case 'scissors vs rock':
+      console.log('YOU LOSE');
+      computerScore++;
+      cScore.textContent = `${computerScore}`;
+      comment.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
+      break;
+    default:
+      console.log('Tie');
+      comment.textContent = 'It\'s a Tie';
   }
-  if (game === 'lost') {
-    computerScore++
-    cScore.textContent = `${computerScore}`
-  }
-};
-
-function isGameOver(){
-  if (playerScore === 5 || computerScore === 5) {
-    if (playerScore > computerScore){
-      comment.textContent = 'You Won The Game'
-      return true
-    } else {
-      comment.textContent = 'You Lost The Game'
-      return true
-    }
-  }};
-
-const rock = document.querySelector('#rock');
-const paper = document.querySelector('#paper');
-const scissors = document.querySelector('#scissors');
+}
 
 
 rock.addEventListener('click',function(){
-  if(isGameOver()) return;
-  let selection = rock.getAttribute('id');
-  console.log(selection);
-  let game = playRound(computerSelect(),selection);
-  getScore(game);
+  playRound('rock')
 });
 
 paper.addEventListener('click',function(){
-  if(isGameOver()) return;
-  let selection = paper.getAttribute('id');
-  console.log(selection);
-  let game = playRound(computerSelect(),selection);
-  getScore(game);
+  playRound('paper')
 });
 
 scissors.addEventListener('click',function(){
-  if(isGameOver()) return;
-  let selection = scissors.getAttribute('id');
-  console.log(selection);
-  let game = playRound(computerSelect(),selection);
-  getScore(game);
+  playRound('scissors')
 });
-
-
-
-
-
-
-
-
-
 
 
